721545740c80de78d874063e86bcf4e18212bbe6
Marc-24-bb <marcalexandre2703@gmail.com>
2024-02-05 11:17:55 -0500

    V1

--- transformation_geometrique.py
+++ transformation_geometrique.py
@@ -1,3 +1,6 @@
+import math
+
+
 def calculer_reflexion_point(tuple, axe):
     reflexion_point_x = None
     reflexion_point_y = None
@@ -8,3 +11,22 @@ def calculer_reflexion_point(tuple, axe):
         reflexion_point_x = tuple[0] * -1
         reflexion_point_y = tuple[1]
     return reflexion_point_x, reflexion_point_y
+
+def calculer_rotate_point(tuple_p, tuple_c, angle):
+    distance = 0
+    distance = ((tuple_p[0]-tuple_c[0])**2 + (tuple_p[1]-tuple_c[1])**2)**0.5
+    angle_depart = 0
+    angle_depart = math.degrees(math.atan((tuple_p[1]-tuple_c[1])/(tuple_p[0]-tuple_c[0])))
+    angle_final = 0
+    angle_final = angle + angle_depart
+
+    if angle_final < 0:
+        angle_final +=360
+
+    angle_final_rad = math.radians(angle_final)
+    coord_x = math.cos(angle_final_rad) * distance
+    coord_y = math.sin(angle_final_rad) * distance
+
+    return coord_x, coord_y
+resultat = calculer_rotate_point((2,4), (0,0), -45)
+print(resultat)
