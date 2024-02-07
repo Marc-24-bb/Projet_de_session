9cbe46d789e41827c02369c567476b4e9b3ef4ff
marc-24-bb <marcalexandre2703@gmail.com>
2024-02-07 17:34:23 -0500

    V2

---
+++ test_coordonnees_clou.py
@@ -0,0 +1,5 @@
+from coordonnees_clou import calculer_coordonnees_clou
+
+def test_calculer_coordonnes_clou():
+    calculer_coordonnees_clou(a,b,c,d,e)
+print(calculer_coordonnees_clou(3,10,1,0.75,2))
